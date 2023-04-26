2b14c4d44419740b87fa4eebbb775c2343bfe5b2
DESKTOP-LVRUHT0\NPN <NPNc>
2023-04-26 23:56:01 +0700

    init project

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * ZizyTheme Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package ZizyTheme
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_ZIZYTHEME_VERSION', '1.0.0' );
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style( 'zizytheme-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ZIZYTHEME_VERSION, 'all' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
