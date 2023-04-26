9bddcc1da0edabcaad4a14517db5b0afb6783e7c
DESKTOP-LVRUHT0\NPN <NPNc>
2023-04-27 03:22:43 +0700

    update header

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,39 +23,47 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'zizy-theme' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'zizy-theme'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$zizy_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $zizy_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $zizy_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
+				<div class="logo-container">
+					<?php
+					if (has_custom_logo()) :
+						the_custom_logo();
+					else :
+					?>
+						<a href="<?php echo home_url() ?>" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+							<img class="rounded mx-auto d-block" src="<?php echo get_template_directory_uri() . '/assets/img/Zizylo1.png' ?>" />
+						</a>
+					<?php
+					endif;
+					?>
+				</div>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'zizy-theme' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+			<div class="container-fluid d-flex align-items-center justify-content-center primary-menu">
+				<nav id="site-navigation" class="main-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'zizy-theme'); ?></button>
+					<?php
+					if (has_nav_menu('primary')) :
+						wp_nav_menu([
+							'theme_location' => 'primary',
+							'container'      => false,
+							'menu_class'     => 'text-jura',
+							'menu_id'        => '',
+							'depth'          => 3
+						]);
+					else :
+						printf(
+							'<a href="%1$s" class="text-jura">%2$s</a>',
+							esc_url(admin_url('/nav-menus.php')),
+							esc_html__('Asign a menu', 'Zizy')
+						);
+					endif;
+					?>
+				</nav><!-- #site-navigation -->
+			</div>
+		</header><!-- #masthead -->
